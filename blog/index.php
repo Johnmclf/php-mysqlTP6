c218992c5c763ad8061959ab2ab002147160a31a
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-10-22 09:34:53 +0200

    Mettez à jour le modèle

--- blog/index.php
+++ blog/index.php
@@ -1,12 +1,8 @@
 <?php 
+
 require('src/model.php');
-if (isset($_GET['id']) && $_GET['id'] > 0) {
-$identifier = $_GET['id'];
-} else {
-echo 'Erreur : aucun identifiant de billet envoyé';
-die;
-}
-$post = getPost($identifier);
-$comments = getComments($identifier);
-require('templates/post.php');
-    
+
+$posts = getPosts();
+
+require('templates/homepage.php');
+
